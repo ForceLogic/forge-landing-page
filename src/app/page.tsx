94c26c0618208d307c89a528349d185143f009fe
MiguelAngelBusto <chetobustos96@gmail.com>
2026-02-22 17:06:46 -0300

    feat: add footer and complete level 1 visual structure

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,14 @@
 import Hero from "@/components/Hero";
 import Services from "@/components/Services"; // Importamos el nuevo componente
 import Contact from "@/components/Contact"; // 1. Importar
-
+import Footer from "@/components/Footer";
 export default function Home() {
   return (
     <main className="min-h-screen bg-black">
       <Hero />
       <Services />
       <Contact />
-      <div className="h-20 bg-black" />
+            <Footer />
     </main>
   );
 }
